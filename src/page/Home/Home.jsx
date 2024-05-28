43ad5a2ed6307b30ba1aae1d8abf469b051085fc
Shahobbek <teshayevshahob023@gmail.com>
2024-05-28 19:23:32 +0500

    Adding

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+import MainAPI from "../../components/mainData/MainAPI";
 
 const Home = () => {
-
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <MainAPI />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
